318bded58c51567f8016f4aa2068b90d9f47f629
Abhradeep Mukherjee <abhradeep22g@gmail.com>
2024-06-05 23:52:39 +0530

    changes in babelrc, babelconfig and tsconfig.json

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,3 @@
 module.exports = {
-    presets: [
-      'next/babel',
-      '@babel/preset-env',
-      '@babel/preset-react',
-      '@babel/preset-typescript'
-    ],
-    plugins: [
-      '@babel/plugin-proposal-class-properties',
-      '@babel/plugin-proposal-object-rest-spread'
-    ]
-  };
-  
+  presets: ['next/babel']
+};
